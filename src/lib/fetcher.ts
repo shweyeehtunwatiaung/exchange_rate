68551d887016e2faa45d193faa58e19f6e6ea9cd
shweyeehtunwatiaung <jessica.sy.dev@gmail.com>
2024-01-22 18:57:53 +0630

    update currency endpoint

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,17 +1,57 @@
+// import { AxiosRequestConfig } from "axios";
+// import appAxios from "./axios";
+
+// export const fetcher = (
+//   url: string,
+//   config?: {
+//     baseUrl?: string;
+//     headers?: AxiosRequestConfig["headers"];
+//   }
+// ) => {
+//   if (config?.baseUrl) {
+//     appAxios.defaults.baseURL = config?.baseUrl;
+//   }
+//   return appAxios
+//     .get(url, { headers: config?.headers })
+//     .then((res) => res.data);
+// };
+
 import { AxiosRequestConfig } from "axios";
 import appAxios from "./axios";
+interface RequestConfig extends AxiosRequestConfig {
+  apikey?: string;
+}
 
 export const fetcher = (
   url: string,
   config?: {
     baseUrl?: string;
     headers?: AxiosRequestConfig["headers"];
+    apikey?: string;
   }
 ) => {
+  const axiosConfig: RequestConfig = {
+    headers: {
+      ...config?.headers
+    }
+  };
+
   if (config?.baseUrl) {
-    appAxios.defaults.baseURL = config?.baseUrl;
+    axiosConfig.baseURL = config.baseUrl;
   }
+
+  if (config?.apikey && !config.baseUrl) {
+    axiosConfig.headers = {
+      ...axiosConfig.headers,
+      apikey: config.apikey
+    };
+  }
+
   return appAxios
-    .get(url, { headers: config?.headers })
-    .then((res) => res.data);
+    .get(url, axiosConfig)
+    .then((res) => res.data)
+    .catch((error) => {
+      console.log("error", error);
+      throw error; // Re-throw the error to handle it in the calling code
+    });
 };
--- src/services/currencies.ts
+++ src/services/currencies.ts
@@ -3,19 +3,19 @@ import { routeFilter } from "@/utils";
 import useSWR from "swr";
 
 export const useGetCurrencies = () =>
-  useSWR(
-    `/list?${routeFilter({
-      access_key: process.env.NEXT_PUBLIC_CURRENCY_ACCESS_KEY,
-    })}`
+  useSWR(`/currency_data/list`, (url) =>
+    fetcher(url, {
+      apikey: process.env.NEXT_PUBLIC_CURRENCY_ACCESS_KEY
+    })
   );
 
 export const useGetRates = () =>
   useSWR(
     `/rates?${routeFilter({
-      key: process.env.NEXT_PUBLIC_CURRENCY_RATES_ACCESS_KEY,
+      key: process.env.NEXT_PUBLIC_CURRENCY_RATES_ACCESS_KEY
     })}`,
     (url) =>
       fetcher(url, {
-        baseUrl: process.env.NEXT_PUBLIC_CURRENCY_RATES_API,
+        baseUrl: process.env.NEXT_PUBLIC_CURRENCY_RATES_API
       })
   );
